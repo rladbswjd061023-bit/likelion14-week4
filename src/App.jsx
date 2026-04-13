f55785a2e2596d259d21e1fd0cfa0e510aed94f8
rladbswjd061023-bit <rladbswjd061023@gmail.com>
2026-04-13 20:12:51 +0900

    :tada: Initial project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+//
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import RootLayout from "../src/layout/RootLayout.jsx";
+import Main from "../src/pages/Main/Main.jsx"
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+    <Route element={<RootLayout />}>
+    <Route path="/" element={<Main />} />
+    </Route>
+    </Routes>
+    </BrowserRouter>
+  );
+}
+export default App;
